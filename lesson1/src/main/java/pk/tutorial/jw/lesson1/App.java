32d900f7e3613c3dfe3e77b0666c5704a6b39a60
LeonJr <LeonJr@mattthew>
2013-01-13 20:14:27 +0100

    Created module

---
+++ lesson1/src/main/java/pk/tutorial/jw/lesson1/App.java
@@ -0,0 +1,11 @@
+package pk.tutorial.jw.lesson1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ){
+        System.out.println( "Hello Maven!" );
+    }
+}
